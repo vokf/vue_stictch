1b1e869559b377ee189ad33c17627f4a136e65b5
demo <2297802467@qq.com>
2020-09-19 20:48:24 +0800

    index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home'
+import App from '@/App'
+import Login from '@/components/Login'
+import Register from '@/components/Register'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    component: App,
+    meta: { title: '首页' }
+  },
+  {
+    path: '/home',
     component: Home,
-    hidden: true
+    meta: { title: '' }
   },
   {
     path: '/about',
@@ -18,7 +25,25 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue')
+      import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    meta: {
+      title: '',
+      login_require: true
+    }
+  },
+  {
+    path: '/login',
+    component: Login,
+    meta: {
+      title: '登录'
+    }
+  },
+  {
+    path: '/register',
+    component: Register,
+    meta: {
+      title: '注册'
+    }
   }
 ]
 
@@ -26,5 +51,16 @@ const router = new VueRouter({
   routes,
   mode: 'history'
 })
-
+router.beforeEach((to, from, next) => {
+  window.document.title = to.meta.title
+  if (
+    to.matched.some(item => {
+      return item.meta.login_require
+    })
+  ) {
+    next('/login')
+  } else {
+    next()
+  }
+})
 export default router
