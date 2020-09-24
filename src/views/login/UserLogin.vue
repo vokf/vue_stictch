115eabaa9b0a34128975c5008d86937b457ff0eb
demo <2297802467@qq.com>
2020-09-24 09:00:54 +0800

    UserLogin.vue

---
+++ src/views/login/UserLogin.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div>
+      <div>
+        <div>
+          <Login />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Login from '@/components/Login'
+
+export default {
+  components: {
+    Login
+  },
+  name: 'UserLogin'
+}
+</script>
+
+<style scoped></style>
