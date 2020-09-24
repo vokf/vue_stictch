8dd534b05b73975c21a4691f99285fa427cc1086
demo <2297802467@qq.com>
2020-09-24 09:01:18 +0800

    UserRegister.vue

---
+++ src/views/register/UserRegister.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>
+      <div>
+        <Register />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Register from '@/components/Register'
+export default {
+  name: 'UserRegister',
+  components: {
+    Register
+  }
+}
+</script>
+
+<style scoped type="scss"></style>
