b9b4ba934b6104b40eb544f6cb8c811961502673
demo <2297802467@qq.com>
2020-09-24 10:59:18 +0800

    Login.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -8,7 +8,7 @@
         <el-form>
           <el-form-item>
             <el-input
-              placeholder="账户名"
+              placeholder="用户名"
               auto-complete="on"
               v-model="user.userName"
               clearable
@@ -30,10 +30,12 @@
           </el-form-item>
           <el-form-item>
             <el-button
+              :loading="logining"
+              @click="submit"
               :style="{ background: 'rgb(190, 200, 200)' }"
               style="margin-left: 20px"
-              >提交</el-button
-            >
+              >提交
+            </el-button>
           </el-form-item>
         </el-form>
       </el-main>
@@ -42,20 +44,30 @@
 </template>
 
 <script>
+import http from '@/data/http'
+
 export default {
   name: 'Login',
   data() {
     return {
+      logining: false,
       // form 表单的input 数据
       user: {
         userName: '',
         password: ''
       },
-      check: false
+      check: false,
+      rules: {}
     }
   },
-  mounted() {},
+  mounted() {
+    const that = this
+    that.account()
+  },
   methods: {
+    /**
+     * 获取cookie
+     */
     getCookie: () => {
       if (document.cookie.length > 0) {
         const arr = document.cookie.split('; ')
@@ -72,25 +84,58 @@ export default {
         this.check = true
       }
     },
+    /**
+     * 清除 cookie
+     */
     clearCookie: () => {
       this.setCookie('', '', -1) //修改2值都为空，天数为负1天就好了
     },
+
+    /**
+     * 设置cookie
+     * @param c_name 账号
+     * @param c_pwd 密码
+     * @param c_day 更新时间
+     */
     setCookie: (c_name, c_pwd, c_day) => {
       const exdate = new Date() //获取时间
       exdate.setTime(exdate.getTime() + 24 * 60 * 60 * 1000 * c_day) //保存的天数
-      //字符串拼接cookie
-      window.document.cookie =
-        'userName' +
-        '=' +
-        c_name +
-        ';path=/;expires=' +
-        exdate.toLocaleDateString()
-      window.document.cookie =
-        'password' +
-        '=' +
-        c_pwd +
-        ';path=/;expires=' +
-        exdate.toLocaleDateString()
+      document.cookie =
+        'userName=' + c_name + ';path=/;expires=' + exdate.toLocaleString()
+      document.cookie =
+        'password=' + c_pwd + ';path=/;expires=' + exdate.toLocaleString()
+    },
+    /**
+     * 登录
+     */
+    submit: () => {
+      const that = this
+      that.$ref.user.validate(valid => {
+        if (valid) {
+          that.logining = true
+          if (that.check === true) {
+            that.setCookie(that.user.userName, that.user.password, 7)
+          } else {
+            that.clearCookie()
+          }
+          http
+            .fetchGet('/user/', {
+              userName: that.user.userName,
+              password: that.user.password
+            })
+            .then(res => {
+              console.log(res)
+            })
+        }
+      })
+    },
+    account() {
+      if (document.cookie.length <= 0) {
+        const that = this
+        console.log(that.getCookie('userName'))
+        that.user.userName = that.getCookie('userName')
+        that.user.password = that.getCookie('password')
+      }
     }
   }
 }
