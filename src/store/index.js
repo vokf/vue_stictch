3213392023f7ed666102ca47a924b0d5742999fb
demo <2297802467@qq.com>
2020-09-29 22:51:32 +0800

    index.js

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
-
+export const LOGIN = 'LOGIN'
+export const USERINFO = 'USERINFO'
+export const LOGOINSTATUS = 'LOGINSTATUS'
 export default new Vuex.Store({
   state: {},
   mutations: {},
