8eccba9714a34e718579b1f779bcf9e0697d5229
demo <2297802467@qq.com>
2020-09-29 22:32:32 +0800

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -30,6 +30,8 @@ export default {
 <style scoped>
 * {
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   margin: 0;
   padding: 0;
   box-sizing: border-box;
