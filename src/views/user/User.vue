f543b4a892d182539789fd8f3c1da0f0c55a0ea0
demo <2297802467@qq.com>
2020-10-04 10:07:17 +0800

    view

---
+++ src/views/user/User.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div>
+      <div><User /></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import User from '@/components/user/User'
+export default {
+  name: 'Detail',
+  components: {
+    User
+  }
+}
+</script>
+
+<style scoped></style>
