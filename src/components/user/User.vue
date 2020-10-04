31ba80e8c752d395f38591f38a007127e053daed
demo <2297802467@qq.com>
2020-10-04 10:07:01 +0800

    component

---
+++ src/components/user/User.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div>
+      <div>
+        <div class="center">
+          <div>dfijdfi</div>
+          <el-table> </el-table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'userDetail',
+  data() {
+    return {
+      user: {},
+      userList: []
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.center {
+}
+</style>
