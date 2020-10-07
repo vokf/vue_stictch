9bf812a8c35ad8601334c253e3e681963f1ecbaa
demo <2297802467@qq.com>
2020-10-07 23:08:27 +0800

    component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,9 +2,9 @@
   <div class="">
     <div>
       <div>
-        <el-container style="background: #c0cee9">
+        <el-container style="background: #3f4041">
           <el-header height="50px">
-            <el-breadcrumb separator-class="el-icon-arrow-right" class="bread">
+            <el-breadcrumb separator="/" class="bread">
               <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
               <el-breadcrumb-item><a href=""></a>用户</el-breadcrumb-item>
               <el-breadcrumb-item>修改用户 </el-breadcrumb-item>
@@ -16,9 +16,13 @@
       </div>
       <div style="background: #ff6c6c">
         <el-container>
-          <el-header height="50px">
-            <el-input v-model="search" size="80"></el-input>
-          </el-header>
+          <el-header height="50px"> </el-header>
+        </el-container>
+      </div>
+      <div style="background: #e6a23c;height: 100px">
+        <el-container class="input">
+          <el-input placeholder="请输入" v-model="search" />
+          <el-button type="primary">搜索</el-button>
         </el-container>
       </div>
     </div>
@@ -50,6 +54,13 @@ export default {
   right: 200px;
   top: 20px;
   padding: {
+    top: 20px;
+  }
+}
+.input {
+  width: 600px;
+  padding: {
+    left: 200px;
     top: 20px;
   }
 }
