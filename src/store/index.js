133d0cffeff9a3f1d15c5691f1d0acddb399293d
demo <2297802467@qq.com>
2020-10-09 19:17:50 +0800

    store

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,42 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  plugins: [
+    createPersistedState({
+      storage: window.localStorage
+    })
+  ],
   //data
+  // 用户信息
   state: {
-    userId: 0,
-    theUserName: ''
+    userId: 1,
+    userName: '',
+    age: 0,
+    gender: '',
+    phone: '',
+    companyName: '',
+    companyLocation: '',
+    email: '',
+    special: '',
+    roleName: ''
   },
   //methods
   mutations: {
     updateUserId(state, num) {
       state.userId = num.id
+      // localStorage.setItem('userId', state.userId)
+
       console.log('mutation之后呢' + state.userId)
     },
     setUserId(state, info) {
       state.userId = info.userId
+    },
+    change(state, pl) {
+      state.userId += pl.num
     }
   },
   actions: {
