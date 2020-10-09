e9b300833ff93965213a1453c06daa1ec78764ed
demo <2297802467@qq.com>
2020-10-09 19:18:27 +0800

    component

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -6,7 +6,7 @@
           <el-header height="100px" style="background: #b6beb2">
             <div class="right">欢迎</div>
           </el-header>
-          <el-main style="background: #50bfbf"> gg</el-main>
+          <el-main style="background: #50bfbf"></el-main>
         </div>
       </div>
     </div>
@@ -18,44 +18,46 @@ import { mapState } from 'vuex'
 
 export default {
   name: 'User',
-  theUser: '',
   data() {
     return {
-      // 用户信息
-      user: {
-        userName: '',
-        password: '',
-        age: 0,
-        gender: '',
-        phone: '',
-        companyName: '',
-        companyLocation: '',
-        email: '',
-        special: '',
-        roleName: ''
-      },
       userList: []
     }
   },
   created() {
     let that = this
-    that.$axios({
-      url: 'http://localhost:8090/user/findById',
-      params: {
-        id: that.userId
-      },
-      // params: { id: Number(that.$store.state.userId) },
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        dataType: 'json'
-      }
-    })
+    that
+      .$axios({
+        url: 'http://localhost:8090/user/findById',
+        params: {
+          userId: that.userId
+        },
+        // params: { id: Number(that.$store.state.userId) },
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          dataType: 'json'
+        }
+      })
+      .then(res => {
+        let newVar = (that.userList = res.data)
+        console.log(newVar)
+      })
   },
   computed: {
-    ...mapState(['userId'])
-  }
+    ...mapState([
+      'userId',
+      'userName',
+      'age',
+      'gender',
+      'phone',
+      'companyName',
+      'companyLocation',
+      'email',
+      'special'
+    ])
+  },
+  methods: {}
 }
 </script>
 
