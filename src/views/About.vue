2f9b14889ab1b9f601fd8127b38101be52011480
demo <2297802467@qq.com>
2020-10-09 19:17:33 +0800

    test

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>{{ userId }}</h2>
+    <button @click="change({ num: 8 })">fff</button>
   </div>
 </template>
+<script>
+import { mapState, mapMutations } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState(['userId'])
+  },
+  created() {
+    let item = localStorage.getItem('userId')
+    console.log(item)
+  },
+  methods: {
+    ...mapMutations(['change'])
+  }
+}
+</script>
