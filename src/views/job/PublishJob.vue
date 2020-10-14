a60ac6c9781ba8fc8d9ea8a1906439d582b0171b
demo <2297802467@qq.com>
2020-10-14 18:50:18 +0800

    view

---
+++ src/views/job/PublishJob.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>
+      <publish-job />
+    </div>
+  </div>
+</template>
+
+<script>
+import publishJob from '@/components/job/publishJob'
+export default {
+  name: 'PublishJob',
+  components: { publishJob }
+}
+</script>
+
+<style lang="scss" scoped></style>
