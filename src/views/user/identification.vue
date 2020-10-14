1db57de1e5f00fd95d4e6422721f18e5418c17a8
demo <2297802467@qq.com>
2020-10-14 18:50:02 +0800

    view

---
+++ src/views/user/identification.vue
@@ -0,0 +1,16 @@
+<template>
+  <div><ident-user /></div>
+</template>
+
+<script>
+import identUser from '@/components/user/identUser'
+export default {
+  name: 'identification',
+  components: {
+    identUser
+  },
+  methods: {}
+}
+</script>
+
+<style scoped></style>
