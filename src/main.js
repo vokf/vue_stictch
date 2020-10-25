a49bf31f3ccf4cd3ab2e7bdf7b3da6db96e323d7
demo <2297802467@qq.com>
2020-10-25 22:38:38 +0800

    main.js

--- src/main.js
+++ src/main.js
@@ -7,7 +7,13 @@ import axios from 'axios'
 import('./assets/scss/style.scss')
 
 Vue.use(ElementUI)
+import VueQuillEditor from 'vue-quill-editor'
+// require styles 引入样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 
+Vue.use(VueQuillEditor)
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios
 
