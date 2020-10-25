c90cc3f5c4af8f4a794895622738b18b054e0c4b
demo <2297802467@qq.com>
2020-10-25 22:34:50 +0800

    view

---
+++ src/views/job/allPublishJob.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div><Header /></div>
+    <div><all-job /></div>
+  </div>
+</template>
+
+<script>
+import allJob from '@/components/job/allJob'
+import Header from '@/views/Header'
+export default {
+  name: 'allPublishJob',
+  components: {
+    allJob,
+    Header
+  }
+}
+</script>
+
+<style scoped></style>
