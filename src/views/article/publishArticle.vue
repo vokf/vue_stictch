4605321ae19c92b1ce0de6391d06a79325eb6a23
demo <2297802467@qq.com>
2020-10-25 22:48:32 +0800

    view

---
+++ src/views/article/publishArticle.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div>
+      <Header />
+    </div>
+    <div>
+      <Article />
+    </div>
+  </div>
+</template>
+
+<script>
+import Article from '@/components/article/Article'
+import Header from '@/views/Header'
+
+export default {
+  name: 'publishArticle',
+  components: {
+    Article,
+    Header
+  }
+}
+</script>
+
+<style scoped></style>
