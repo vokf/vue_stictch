182a3f7b131f7064e600861ef19cfa73714c63fe
demo <2297802467@qq.com>
2020-11-04 08:53:37 +0800

    component

---
+++ src/components/user/Info.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div><Header /> <Nav /></div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Header from '@/views/Header'
+import Nav from '@/components/user/Nav'
+export default {
+  name: 'Info',
+  components: { Nav, Header }
+}
+</script>
+
+<style scoped></style>
