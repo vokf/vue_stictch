0f4f6cdeb3bb4833d293c9e7627617442427f101
demo <2297802467@qq.com>
2020-11-10 13:35:28 +0800

    component

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -1,46 +1,47 @@
 <template>
   <div>
     <div>
-      <el-header class="head">
+      <div class="head">
         <div class="header">
           <el-menu
-            background-color="#f2f6f2"
-            :default-active="activeIndex"
-            router
+            background-color=" #c6c6f3"
             mode="horizontal"
-            active-text-color="#59b5e5"
+            active-text-color="#c6c6f3"
+            router
+            :default-active="activeIndex"
           >
-            <el-menu-item index="1" route="index">首页</el-menu-item>
-            <el-menu-item index="2" route="tt">热点</el-menu-item>
-            <el-menu-item index="3">处理</el-menu-item>
-            <el-menu-item index="4">发布文章</el-menu-item>
-            <el-menu-item>
-              <div>嘿嘿</div>
-            </el-menu-item>
+            <el-menu-item index="/index"><span>首页</span></el-menu-item>
+            <el-menu-item><span>热点</span></el-menu-item>
+            <el-menu-item index="/publishArticle"
+              ><span>发布文章</span></el-menu-item
+            >
           </el-menu>
         </div>
-        <!--        <div class="btn">-->
-        <!--          <el-button>成都</el-button>-->
-        <!--        </div>-->
+
         <div class="userName">
           <span v-if="userName != ''">
-            <span class="uu">
-              <el-button
-                ><el-link href="/user">{{ userName }}</el-link></el-button
-              ></span
-            >
-            <el-button class="btn">退出</el-button>
+            <el-button class="btn" @click="logout">退出</el-button>
           </span>
           <span v-else-if="userName === ''">
-            <el-button class="btn">登录</el-button>
+            <el-button class="btn" @click="clickUser">登录</el-button>
+          </span>
+          <span>
+            <el-button
+              class="register"
+              @click="toRegister"
+              v-show="registerShow"
+              >注册</el-button
+            >
           </span>
 
-          <!--          <el-button @click="clickUser" v-if="userName != ''"-->
-          <!--            >{{ userName }}-->
-          <!--          </el-button>-->
+          <el-image
+            :src="userPicture"
+            @click="clickImage"
+            v-show="boolShow"
+          ></el-image>
         </div>
-      </el-header>
-      <div style="height: 40px"></div>
+      </div>
+      <div style="height:90px"></div>
     </div>
   </div>
 </template>
@@ -50,41 +51,88 @@ import { mapState } from 'vuex'
 
 export default {
   name: 'Header',
+
   data() {
     return {
+      registerShow: false,
+      boolShow: false,
       activeIndex: '1'
     }
   },
   computed: {
-    ...mapState(['userName'])
+    ...mapState(['userName', 'userPicture'])
   },
   methods: {
-    clickUser() {}
+    clickUser() {
+      this.$router.push('/login')
+    },
+    clickImage() {
+      this.$router.push('/user/info')
+    },
+    logout() {
+      window.localStorage.clear()
+      this.$router.push('/index')
+    },
+    toRegister() {
+      this.$router.push('/register')
+    }
+  },
+  created() {
+    if (this.userPicture != '') {
+      this.boolShow = true
+    } else {
+      this.boolShow = false
+    }
+    if (this.userName == '') {
+      this.registerShow = true
+    } else {
+      this.registerShow = false
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
 .header {
-  width: 85%;
-  height: 62px;
-  background-color: #f2f6f2;
-  left: 120px;
+  background-color: #c6c6f3;
+  width: 97%;
+  padding: {
+    right: 10px;
+  }
+  top: 0px;
+  height: 100px;
 
   position: fixed;
-
   box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
-}
+  .el-menu {
+    padding: {
+      left: 15px;
+    }
+    width: 100%;
+    border: 1px solid red;
+    position: relative;
+    height: 100px;
+  }
+  .el-menu-item {
+    position: relative;
+    top: 20px;
+    left: 170px;
+    width: 120px;
 
-.menu {
-  width: 250px;
-  display: flex;
-  position: relative;
-  background-color: #59b5e5;
-}
-
-.dd {
-  margin-top: 55px;
+    text-align: center;
+    span {
+      letter-spacing: 2px;
+      font: {
+        weight: bold;
+      }
+      position: relative;
+      //
+    }
+  }
+  .el-menu-item > span:nth-child(2) {
+    position: relative;
+    left: 5px;
+  }
 }
 
 .userName {
@@ -94,22 +142,28 @@ export default {
   top: 15px;
   left: 1000px;
 
-  .uu {
-    font: {
-      weight: bold;
-    }
-  }
-
-  .btn {
+  .register {
     position: fixed;
     top: 15px;
-    left: 1100px;
+    left: 1200px;
   }
 }
-
 .head {
   position: fixed;
-  z-index: 1;
+  z-index: 4000;
   top: 0;
+  .btn {
+    position: fixed;
+    top: 35px;
+    left: 1100px;
+  }
+  .el-image {
+    height: 50px;
+    width: 50px;
+    position: fixed;
+    left: 1000px;
+    top: 30px;
+    border-radius: 70%;
+  }
 }
 </style>
